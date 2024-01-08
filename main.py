feb2efcaa6eab35233c37dd77de1bd3aac5afdc3
Calvin Matson-Roberts <calvinmatsonroberts@outlook.com>
2024-01-08 16:22:12 -0700

    fixed domain slice

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ email = input("What is your email address?: ").strip()
 # slice out user name
 user_name = email[:email.index("@")]
 # slice out domain name
-domain = email[email.index("@"):]
+domain = email[email.index("@") + 1:]
 # format message
 message = "Your username is {} and your domain name is {}"
 output = message.format(user_name, domain)
